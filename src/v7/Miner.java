c417b6d40378d30890dcb0dc2b7f5a125481cf1d
Ethan Wong <fireshot90@gmail.com>
2022-01-16 20:53:46 -0500

    v7

---
+++ src/v7/Miner.java
@@ -0,0 +1,262 @@
+package v7;
+
+import org.apache.commons.io.input.BoundedInputStream;
+
+import battlecode.common.*;
+
+public class Miner extends RobotPlayer {
+
+    static int prevIncome = 0;
+    static boolean aboveHpThresh = true;
+    static int turnsAlive = 0;
+    static MapLocation[] sectorMdpts = new MapLocation[49];
+    static Direction bounceDir = null;
+    static int maxTravelDistance = 100;
+    static Direction spawnDir = null;
+    static int sectorNumber = -1;
+    static MapLocation home = null;
+    static MapLocation closestEnemyArchon = null;
+    static int scoutPattern = 0;
+    static MapLocation scoutTgt = null;
+    /**
+     * Run a single turn for a Miner.
+     * This code is wrapped inside the infinite loop in run(), so it is called once per turn.
+     */
+
+    static Direction stallOnGoodRubble(RobotController rc) throws GameActionException {
+        MapLocation src = rc.getLocation();
+        int currRubble = rc.senseRubble(src);
+          int minRubble = currRubble;
+          MapLocation minRubbleLoc = src;
+          if (currRubble > 0) {
+            for (Direction d : Direction.allDirections()) {
+              MapLocation test = src.add(d);
+              if (rc.onTheMap(test) && !rc.isLocationOccupied(test) && rc.canSenseLocation(test)) {
+                int rubbleHere = rc.senseRubble(test);
+                if (rubbleHere <= minRubble) {
+                  minRubble = rubbleHere;
+                  minRubbleLoc = test;
+                }
+              }
+            }
+          }
+          return src.directionTo(minRubbleLoc);
+      }
+
+    static boolean isHostile(RobotInfo enemy) {
+        return (enemy.getType() == RobotType.SOLDIER || enemy.getType() == RobotType.SAGE 
+        || enemy.getType() == RobotType.WATCHTOWER);
+      }
+
+    //action flow:
+    //1. mine
+    //2. if close enemy run away
+    //3. if close to resource go
+    //4. if close to other miners spread
+    //5. goto nearest sector with resource
+    static void runMiner(RobotController rc) throws GameActionException {
+        int income = 0;
+        int senseRadius = rc.getType().visionRadiusSquared;
+        Team friendly = rc.getTeam();
+        Team opponent = rc.getTeam().opponent();
+        MapLocation src = rc.getLocation();
+        Direction dir = null;
+        MapLocation resources = null;
+        int mapArea = rc.getMapHeight() * rc.getMapWidth();
+
+        if (turnsAlive == 0) {
+            for (int i = 48; i >= 0; i --) {
+                sectorMdpts[i] = Comms.sectorMidpt(rc, i);
+            }
+            RobotInfo[] spawn = rc.senseNearbyRobots(2, friendly);
+            for (int i = spawn.length - 1; i >= 0; i --) {
+                RobotInfo robot = spawn[i];
+                if (robot.getType() == RobotType.ARCHON) {
+                    spawnDir = rc.getLocation().directionTo(robot.location).opposite();
+                    home = robot.getLocation();
+                    break;
+                }
+            }
+            scoutPattern = rc.readSharedArray(50) % 3;
+            bounceDir = spawnDir;
+            scoutTgt = new MapLocation(rc.getMapWidth() - 1 - src.x, rc.getMapHeight() - 1 - src.y);
+        }
+
+        int distanceFromSpawn = 0;
+        boolean nearbyEnemy = false;
+        boolean nearbyFriend = true;
+        if (home != null) {
+            distanceFromSpawn = rc.getLocation().distanceSquaredTo(home);
+        }
+        RobotInfo[] enemies = rc.senseNearbyRobots(senseRadius, opponent);
+        if (enemies.length > 0) {
+            nearbyEnemy = true;
+        }
+        RobotInfo[] friendlies = rc.senseNearbyRobots(senseRadius, friendly);
+        if (friendlies.length == 0) {
+            nearbyFriend = false;
+        }
+
+        // Try to mine on squares around us.
+        MapLocation me = rc.getLocation();
+        for (int dx = -1; dx <= 1; dx++) {
+            for (int dy = -1; dy <= 1; dy++) {
+                MapLocation mineLocation = new MapLocation(me.x + dx, me.y + dy);
+                // Notice that the Miner's action cooldown is very low.
+                // You can mine multiple times per turn!
+                while (rc.canMineGold(mineLocation)) {
+                    rc.mineGold(mineLocation);
+                    income += 5;
+                }
+                boolean shouldContinue = nearbyEnemy && distanceFromSpawn > (.75 * Math.sqrt(mapArea)) * (.75 * Math.sqrt(mapArea));
+                int stopMiningThres = 1;
+                if (shouldContinue) {
+                    stopMiningThres = 0;
+                }
+                while (rc.canMineLead(mineLocation) && rc.senseLead(mineLocation) > stopMiningThres) {
+                    rc.mineLead(mineLocation);
+                    income += 1;
+                }
+            }
+        }
+
+        //run away from enemy attackers if we see them
+        MapLocation closestAttacker = null;
+        MapLocation[] nearbyLead = rc.senseNearbyLocationsWithLead(senseRadius, 5);
+        MapLocation[] nearbyGold = rc.senseNearbyLocationsWithGold(senseRadius);
+        int nearbyFriendlySoldierCount = 0;
+        MapLocation nearestFriendlySoldier = null;
+        int nearbyFriendlyMinerCount = 0;
+        double highLead = 0;
+        for (int i = enemies.length - 1; i >= 0; i --) {
+            RobotInfo enemy = enemies[i];
+            if ((enemy.getType() == RobotType.SOLDIER || enemy.getType() == RobotType.SAGE 
+            || enemy.getType() == RobotType.WATCHTOWER) && (closestAttacker == null
+            || enemy.location.distanceSquaredTo(src) < closestAttacker.distanceSquaredTo(src))) {
+                closestAttacker = enemy.location;
+            }
+        }
+        for (int i = nearbyLead.length - 1; i >= 0; i --) {
+            double leadCount = (double)rc.senseLead(nearbyLead[i])/(1 + rc.senseRubble(nearbyLead[i])/10.0);
+            if (leadCount > highLead) {
+                highLead = leadCount;
+                resources = nearbyLead[i];
+            }
+        }
+        for (int i = friendlies.length - 1; i >= 0; i --) {
+            RobotInfo friend = friendlies[i];
+            if (isHostile(friend)) {
+                nearbyFriendlySoldierCount ++;
+                if (nearestFriendlySoldier == null || friend.location.distanceSquaredTo(src) < nearestFriendlySoldier.distanceSquaredTo(src)) {
+                    nearestFriendlySoldier = friend.location;
+                }
+            } else if (friend.getType() == RobotType.MINER && (nearestFriendlySoldier == null 
+            || friend.location.distanceSquaredTo(nearestFriendlySoldier) < src.distanceSquaredTo(nearestFriendlySoldier))) {
+                nearbyFriendlyMinerCount ++;
+            }
+        }
+        if (nearbyGold.length > 0) {
+            resources = nearbyGold[0];
+        }
+
+        //main control structure
+        //run if enemy attacker in vision
+        if (closestAttacker != null) {
+            Direction opposite = src.directionTo(closestAttacker).opposite();
+            MapLocation runawayTgt = src.add(opposite).add(opposite);
+            runawayTgt = new MapLocation(Math.min(Math.max(0, runawayTgt.x), rc.getMapWidth() - 1), 
+            Math.min(Math.max(0, runawayTgt.y), rc.getMapHeight() - 1));
+            dir = Pathfinder.getMoveDir(rc, runawayTgt);
+            bounceDir = dir;
+        //if good resources nearby go there
+        } else if (resources != null && (nearbyLead.length + nearbyGold.length) > 2*nearbyFriendlyMinerCount) {
+            dir = Pathfinder.getMoveDir(rc, resources);
+        //else go to sector with reported resources + spread vector + away from closest archon vector
+        } else if (nearbyFriendlySoldierCount > nearbyFriendlyMinerCount) {
+            if (src.distanceSquaredTo(nearestFriendlySoldier) <= 8) {
+                dir = stallOnGoodRubble(rc);
+            } else {
+                dir = Pathfinder.getMoveDir(rc, nearestFriendlySoldier);
+            }
+        } else {
+            if (turnCount % 2 == 0) {
+                sectorNumber = -1;
+                int bestResource = 0;
+                for (int i = 48; i >= 0; i--) {
+                    int[] sector = Comms.readSectorInfo(rc, i);
+                    int distanceToSector = rc.getLocation().distanceSquaredTo(sectorMdpts[i]);
+                    if (sector[2] > bestResource
+                        && maxTravelDistance >= distanceToSector) {
+                        sectorNumber = i;
+                        bestResource = sector[2];
+                    }
+                    if (sector[1] > 0 && (closestEnemyArchon == null 
+                    || src.distanceSquaredTo(closestEnemyArchon) > src.distanceSquaredTo(sectorMdpts[i]))) {
+                        closestEnemyArchon = sectorMdpts[i];
+                    }
+                }
+            }
+            if (sectorNumber != -1 || nearbyFriendlyMinerCount > 1) {
+                double xVector = 0;
+                double yVector = 0;
+                if (sectorNumber != -1) {
+                    dir = rc.getLocation().directionTo(sectorMdpts[sectorNumber]);
+                    xVector = 2*dir.dx;
+                    yVector = 2*dir.dy;
+                }
+                for (int i = friendlies.length - 1; i >= 0; i --) {
+                    MapLocation friendlyLoc = friendlies[i].getLocation();
+                    double d = Math.sqrt(src.distanceSquaredTo(friendlyLoc));
+                    Direction opposite = src.directionTo(friendlyLoc).opposite();
+                    xVector += opposite.dx*(4.0/d);
+                    yVector += opposite.dy*(4.0/d);
+                }
+                MapLocation vectorTgt = src.translate((int)Math.ceil(xVector), (int)Math.ceil(yVector));
+                MapLocation inBounds = new MapLocation(Math.min(Math.max(0, vectorTgt.x), rc.getMapWidth() - 1), 
+                Math.min(Math.max(0, vectorTgt.y), rc.getMapHeight() - 1));
+                dir = Pathfinder.getMoveDir(rc, inBounds);
+                bounceDir = dir;
+            } else {
+                if (scoutPattern == 0) {
+                    if (!rc.onTheMap(src.add(src.directionTo(scoutTgt)))) {
+                        scoutTgt = new MapLocation(rc.getMapWidth() - 1 - src.x, rc.getMapHeight() - 1 - src.y);
+                    }
+                    dir = Pathfinder.getMoveDir(rc, scoutTgt);
+                } else if (scoutPattern == 1) {
+                    if (closestEnemyArchon != null) {
+                        dir = Pathfinder.getMoveDir(rc, closestEnemyArchon);
+                    } else {
+                        dir = Pathfinder.getMoveDir(rc, scoutTgt);
+                    }
+                } else {
+                    if (!rc.onTheMap(src.add(bounceDir))) {
+                        bounceDir = bounceDir.opposite();
+                    }
+                    MapLocation tgt = src.add(bounceDir).add(bounceDir);
+                    MapLocation inBounds = new MapLocation(Math.min(Math.max(0, tgt.x), rc.getMapWidth() - 1), 
+                    Math.min(Math.max(0, tgt.y), rc.getMapHeight() - 1));
+                    dir = Pathfinder.getMoveDir(rc, inBounds);
+                }
+            }
+        }
+        if (rc.canMove(dir)) {
+            rc.move(dir);
+        }
+        //Comms stuff
+        Comms.updateSector(rc, turnCount);
+        int deltaIncome = income - prevIncome;
+        //index 49 is global income
+        int currentIncome = rc.readSharedArray(49);
+        rc.writeSharedArray(49, Math.max(0, currentIncome + deltaIncome));
+        prevIncome = income;
+
+        boolean currentHpThresh = (double)rc.getHealth()/rc.getType().getMaxHealth(1) > 0.2;
+        if (!currentHpThresh && aboveHpThresh) {
+            rc.writeSharedArray(50, rc.readSharedArray(50) - 1);
+        } else if (currentHpThresh && !aboveHpThresh) {
+            rc.writeSharedArray(50, rc.readSharedArray(50) + 1);
+        }
+        aboveHpThresh = currentHpThresh;
+        turnsAlive++;
+    }
+}
---
+++ src/v7/Pathfinder.java
@@ -0,0 +1,168 @@
+package v7;
+
+import battlecode.common.*;
+import mainBot.betterJavaUtil.*;
+
+public class Pathfinder {
+
+    static Direction getMoveDir(RobotController bot, MapLocation tgt) throws GameActionException {
+        MapLocation src = bot.getLocation();
+        int range = 8;
+        bot.setIndicatorString("tgt: " + tgt);
+        
+        //simple heuristic using only nearby 8
+        MapLocation immTgt;
+        if (tgt.isWithinDistanceSquared(src, range) && Clock.getBytecodesLeft() < 4000
+                || Clock.getBytecodesLeft() < 5500) {
+            Direction optimalDir = Direction.CENTER;
+            double optimalCost = 9999;
+            for (Direction dir : Direction.allDirections()) {
+                MapLocation loc = src.add(dir);
+                if (!bot.onTheMap(loc) || bot.isLocationOccupied(loc)) {
+                    continue;
+                }
+                double currCost = Math.sqrt(loc.distanceSquaredTo(tgt))*20 + (bot.senseRubble(loc));
+                if (currCost < optimalCost) {
+                    optimalDir = dir;
+                    optimalCost = currCost;
+                }
+            }
+            return optimalDir;
+
+            //sweep relaxing edges from immediate tgt to src
+        } else {
+            if (tgt.isWithinDistanceSquared(src, range)) {
+                immTgt = tgt;
+            } else {
+                Direction toTgt = src.directionTo(tgt);
+                double radMultiplier = 1;
+                if (toTgt.dx != 0 && toTgt.dy != 0) radMultiplier = 1.0/2;
+
+                //finding translation deltas
+                int deltaX = toTgt.dx*(int)Math.floor(Math.sqrt(range*radMultiplier));
+                int deltaY = toTgt.dy*(int)Math.floor(Math.sqrt(range*radMultiplier));
+
+                //dijkstras in vision target for this turn
+                immTgt = src.translate(deltaX, deltaY);
+            }
+
+            int[][] costs = new int[5][5];
+            for (int i = 4; i >= 0; i --) {
+                for (int j = 4; j >= 0; j --) {
+                    costs[i][j] = -1;
+                }
+            }
+            MapLocation moveTo = null;
+            LinkedList<MapLocation> processQ = new LinkedList<>();
+            int tgtRubble = bot.senseRubble(immTgt);
+            costs[immTgt.x - src.x + 2][immTgt.y - src.y + 2] = tgtRubble;
+            processQ.add(immTgt);
+
+            Node<MapLocation> h = processQ.head;
+
+            while (h != null) {
+                MapLocation current = h.val;
+
+                if (current.equals(src)) {
+                    break;
+                }
+
+                Direction straightDir = current.directionTo(src);
+                int currRubble = costs[current.x - src.x + 2][current.y - src.y + 2];
+                int currCds = currRubble;
+
+                MapLocation left = current.add(straightDir.rotateLeft());
+                if (bot.onTheMap(left) && left.isWithinDistanceSquared(src, range) && (left.equals(src) || !bot.isLocationOccupied(left))) {
+                    int leftRubble = bot.senseRubble(left);
+                    int leftCds = leftRubble;
+                    if (costs[left.x - src.x + 2][left.y - src.y + 2] == -1 || costs[left.x - src.x + 2][left.y - src.y + 2] > leftCds + currCds) {
+                        costs[left.x - src.x + 2][left.y - src.y + 2] = leftCds + currCds;
+                        if (left.equals(src)) {
+                            moveTo = current;
+                        }
+                        if (!processQ.contains(left)) {
+                            processQ.add(left);
+                        }
+                    }
+                }
+
+                MapLocation right = current.add(straightDir.rotateRight());
+                if (bot.onTheMap(right) && right.isWithinDistanceSquared(src, range) && (right.equals(src) || !bot.isLocationOccupied(right))) {
+                    int rightRubble = bot.senseRubble(right);
+                    int rightCds = rightRubble;
+                    if (costs[right.x - src.x + 2][right.y - src.y + 2] == -1 || costs[right.x - src.x + 2][right.y - src.y + 2] > rightCds + currCds) {
+                        costs[right.x - src.x + 2][right.y - src.y + 2] = rightCds + currCds;
+                        if (right.equals(src)) {
+                            break;
+                        }
+                        if (!processQ.contains(right)) {
+                            processQ.add(right);
+                        }
+                    }
+                }
+
+                MapLocation towards = current.add(straightDir);
+                if (bot.onTheMap(towards) && towards.isWithinDistanceSquared(src, range) && (towards.equals(src) || !bot.isLocationOccupied(towards))) {
+                    int toRubble = bot.senseRubble(towards);
+                    int towardsCds = toRubble;
+                    if (costs[towards.x - src.x + 2][towards.y - src.y + 2] == -1 || costs[towards.x - src.x + 2][towards.y - src.y + 2] > towardsCds + currCds) {
+                        costs[towards.x - src.x + 2][towards.y - src.y + 2] = towardsCds + currCds;
+                        if (towards.equals(src)) {
+                            moveTo = current;
+                        }
+                        if (!processQ.contains(towards)) {
+                            processQ.add(towards);
+                        }
+                    }
+                }
+                h = h.next;
+            }
+            if (moveTo == null) {
+                Direction optimalDir = Direction.CENTER;
+                double optimalCost = 9999;
+                for (Direction dir : Direction.allDirections()) {
+                    MapLocation loc = src.add(dir);
+                    if (!bot.onTheMap(loc) || bot.isLocationOccupied(loc)) {
+                        continue;
+                    }
+                    double currCost = Math.sqrt(loc.distanceSquaredTo(tgt))*20 + (bot.senseRubble(loc));
+                    if (currCost < optimalCost) {
+                        optimalDir = dir;
+                        optimalCost = currCost;
+                    }
+                }
+                moveTo = src.add(optimalDir);
+            }
+            return src.directionTo(moveTo);
+        }
+    }
+
+    //return direction away from a location
+    //will try to find a way to maximize distance even if it's not directly away
+    static Direction getAwayDir(RobotController rc, MapLocation loc) throws GameActionException {
+        MapLocation src = rc.getLocation();
+
+        Direction opposite = src.directionTo(loc).opposite();
+
+        MapLocation runawayTgt = src.add(opposite).add(opposite);
+        runawayTgt = new MapLocation(Math.min(Math.max(0, runawayTgt.x), rc.getMapWidth() - 1),
+                Math.min(Math.max(0, runawayTgt.y), rc.getMapHeight() - 1));
+        Direction toReturn = Pathfinder.getMoveDir(rc, runawayTgt);
+
+        if(rc.canMove(toReturn)){
+            return toReturn;
+        }
+        else if(rc.canMove(toReturn.rotateRight() )){
+            return toReturn.rotateRight();
+        }
+        else if(rc.canMove(toReturn.rotateLeft() )){
+            return toReturn.rotateLeft();
+        }
+        else if(rc.canMove(toReturn.rotateLeft().rotateLeft() )){
+            return toReturn.rotateLeft().rotateLeft();
+        }
+        else{
+            return toReturn.rotateRight().rotateRight();
+        }
+    }
+}

5b1943a4eb544ca1139b9de14a062c8a8535a7fb
Andrew Li <achyt28@gmail.com>
2022-01-20 08:51:05 -0500

    removed seed 6147 from rng in RobotPlayer to get randomness
    still, constant seeds are useful for debugging

--- src/mainBot/RobotPlayer.java
+++ src/mainBot/RobotPlayer.java
@@ -23,7 +23,7 @@ public strictfp class RobotPlayer {
      * import at the top of this file. Here, we *seed* the RNG with a constant number (6147); this makes sure
      * we get the same sequence of numbers every time this code is run. This is very useful for debugging!
      */
-    static final Random rng = new Random(6147);
+    static final Random rng = new Random();
 
     /** Array containing all the possible movement directions. */
     static final Direction[] directions = {

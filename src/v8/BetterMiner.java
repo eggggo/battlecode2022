9cc88abc539eb2ec0d6e77ea19b70e88a805faf9
eggggo <geneliu2009@gmail.com>
2022-01-20 16:57:09 -0500

    v8

---
+++ src/v8/BetterMiner.java
@@ -0,0 +1,238 @@
+package v8;
+
+import java.util.Random;
+import battlecode.common.*;
+
+public class BetterMiner extends RobotPlayer {
+
+    static int prevIncome = 0;
+    static boolean aboveHpThresh = true;
+    static int turnsAlive = 0;
+    static MapLocation[] sectorMdpts = new MapLocation[49];
+    static int maxTravelDistance = 100;
+    static MapLocation scoutTgt = null;
+    static Random rng = new Random();
+    static MapLocation nearestFriendlyArchon = null;
+    static MapLocation bestOOVResource = null;
+
+    /**
+     * Run a single turn for a Miner.
+     * This code is wrapped inside the infinite loop in run(), so it is called once per turn.
+     */
+
+    //looks in a 3x3 square around unit to find the best rubble square, then returns direction to it
+    static Direction stallOnGoodRubble(RobotController rc) throws GameActionException {
+        MapLocation src = rc.getLocation();
+        int currRubble = rc.senseRubble(src);
+        int minRubble = currRubble;
+        MapLocation minRubbleLoc = src;
+        if (currRubble > 0) {
+            for (Direction d : Direction.allDirections()) {
+                MapLocation test = src.add(d);
+                if (rc.onTheMap(test) && !rc.isLocationOccupied(test) && rc.canSenseLocation(test)) {
+                    int rubbleHere = rc.senseRubble(test);
+                    if (rubbleHere <= minRubble) {
+                        minRubble = rubbleHere;
+                        minRubbleLoc = test;
+                    }
+                }
+            }
+        }
+        return src.directionTo(minRubbleLoc);
+    }
+
+    //tests whether a unit is hostile(does damage) or not
+    static boolean isHostile(RobotInfo unit) {
+        return (unit.getType() == RobotType.SOLDIER || unit.getType() == RobotType.SAGE 
+        || unit.getType() == RobotType.WATCHTOWER);
+    }
+
+    static void runMiner(RobotController rc) throws GameActionException {
+        int income = 0;
+        int senseRadius = rc.getType().visionRadiusSquared;
+        Team friendly = rc.getTeam();
+        Team opponent = rc.getTeam().opponent();
+        MapLocation src = rc.getLocation();
+        Direction dir = null;
+        MapLocation resources = null;
+        RobotInfo[] enemies = rc.senseNearbyRobots(senseRadius, opponent);
+
+        //setup loop, populates sector mdpts array for cheaper access, gets scout location
+        if (turnsAlive == 0) {
+            for (int i = 48; i >= 0; i --) {
+                sectorMdpts[i] = Comms.sectorMidpt(rc, i);
+            }
+            int scoutPattern = rc.readSharedArray(50) % 3;
+            if (scoutPattern == 0) {
+                scoutTgt = sectorMdpts[rng.nextInt(49)];
+            } else {
+                int designatedLoc = rng.nextInt(5);
+                switch (designatedLoc) {
+                    case 0:
+                        scoutTgt = new MapLocation(0, 0);
+                        break;
+                    case 1:
+                        scoutTgt = new MapLocation(rc.getMapWidth() - 1, 0);
+                        break;
+                    case 2:
+                        scoutTgt = new MapLocation(0, rc.getMapHeight() - 1);
+                        break;
+                    case 3:
+                        scoutTgt = new MapLocation(rc.getMapWidth() - 1, rc.getMapHeight() - 1);
+                        break;
+                    case 4:
+                        scoutTgt = new MapLocation(rc.getMapWidth()/2, rc.getMapHeight()/2);
+                        break;
+                }
+            }
+        }
+
+        //main comms loop, do all comms stuff here
+        //finds mdpt of closest friendly archon, best resources in travel distance
+        //refreshes every 2 turns to reduce bytecode load
+        if (turnCount % 3 == 0) {
+            nearestFriendlyArchon = null;
+            bestOOVResource = null;
+            double bestResource = 0;
+            for (int i = 48; i >= 0; i --) {
+                int[] sectorInfo = Comms.readSectorInfo(rc, i);
+                MapLocation sectorMdpt = sectorMdpts[i];
+                if (sectorInfo[0] == 1 && (nearestFriendlyArchon == null 
+                || sectorMdpt.distanceSquaredTo(src) < nearestFriendlyArchon.distanceSquaredTo(src))) {
+                    nearestFriendlyArchon = sectorMdpt;
+                }
+                if (sectorInfo[2] > 0) {
+                    double score = sectorInfo[2]/Math.sqrt(src.distanceSquaredTo(sectorMdpt));
+                    if (score > bestResource && score >= 0.5) {
+                        bestResource = score;
+                        bestOOVResource = sectorMdpt;
+                    }
+                }
+            }
+        }
+
+        // Try to mine on squares around us.
+        for (int dx = -1; dx <= 1; dx++) {
+            for (int dy = -1; dy <= 1; dy++) {
+                MapLocation mineLocation = new MapLocation(src.x + dx, src.y + dy);
+                // Notice that the Miner's action cooldown is very low.
+                // You can mine multiple times per turn!
+                while (rc.canMineGold(mineLocation)) {
+                    rc.mineGold(mineLocation);
+                    income += 5;
+                }
+                //if we are farther than a certain distance from nearest friendly archon, let's mine out the area
+                int friendlyResourceRadius = (int)Math.pow(Math.max(rc.getMapWidth()/2, rc.getMapHeight()/2), 2);
+                boolean shouldContinue = nearestFriendlyArchon != null 
+                && src.distanceSquaredTo(nearestFriendlyArchon) > friendlyResourceRadius;
+                int stopMiningThres = 1;
+                if (shouldContinue) {
+                    stopMiningThres = 0;
+                }
+                while (rc.canMineLead(mineLocation) && rc.senseLead(mineLocation) > stopMiningThres) {
+                    rc.mineLead(mineLocation);
+                    income += 1;
+                }
+            }
+        }
+
+        //finding closest enemy hostile to run away if exists
+        MapLocation closestAttacker = null;
+        for (int i = enemies.length - 1; i >= 0; i --) {
+            RobotInfo enemy = enemies[i];
+            if (isHostile(enemy) && (closestAttacker == null
+            || enemy.location.distanceSquaredTo(src) < closestAttacker.distanceSquaredTo(src))) {
+                closestAttacker = enemy.location;
+            }
+        }
+
+        //finding best mining spot in vision
+        MapLocation[] nearbyLead = rc.senseNearbyLocationsWithLead(senseRadius, 5);
+        MapLocation[] nearbyGold = rc.senseNearbyLocationsWithGold(senseRadius);
+        int highLead = 0;
+        for (int i = nearbyLead.length - 1; i >= 0; i --) {
+            int leadCount = rc.senseLead(nearbyLead[i]);
+            if (leadCount > highLead) {
+                highLead = leadCount;
+                resources = nearbyLead[i];
+            }
+        }
+        if (nearbyGold.length > 0) {
+            resources = nearbyGold[0];
+        }
+
+        //finding best adj resource with lowest rubble to mine from
+        if (resources != null) {
+            MapLocation lowestRubble = null;
+            int lowestRubbleAmt = 128;
+            for (int i = -1; i <= 1; i ++) {
+                for (int j = -1; j <= 1; j ++) {
+                    MapLocation adjLead = resources.translate(i, j);
+                    if (src.distanceSquaredTo(adjLead) <= senseRadius && rc.onTheMap(adjLead)) {
+                        int currRubble = rc.senseRubble(adjLead);
+                        if (currRubble < lowestRubbleAmt) {
+                            lowestRubble = adjLead;
+                            lowestRubbleAmt = currRubble;
+                        }
+                    }
+                }
+            }
+            resources = lowestRubble;
+        }
+
+        //if you can see the scout target sector mdpt, randomize and go to somewhere else
+        if (src.distanceSquaredTo(scoutTgt) <= senseRadius) {
+            scoutTgt = sectorMdpts[rng.nextInt(49)];
+        }
+
+        //movement action loop, vectors for avoiding enemies
+        int xVector = 0;
+        int yVector = 0;
+        //if good nearby resources go there
+        if (resources != null) {
+            dir = src.directionTo(resources);
+        //if good comms nearby resources go there
+        } else if (bestOOVResource != null) {
+            dir = src.directionTo(bestOOVResource);
+        //else, go to scout location
+        } else {
+            dir = src.directionTo(scoutTgt);
+        }
+        xVector += dir.dx;
+        yVector += dir.dy;
+        
+        //adding away from attacker vector
+        if (closestAttacker != null) {
+            Direction awayAttacker = src.directionTo(closestAttacker).opposite();
+            xVector += 2*awayAttacker.dx;
+            yVector += 2*awayAttacker.dy;
+        }
+
+        MapLocation vectorTgt = src.translate(4*xVector, 4*yVector);
+        MapLocation inBounds = new MapLocation(Math.min(Math.max(0, vectorTgt.x), rc.getMapWidth() - 1), 
+            Math.min(Math.max(0, vectorTgt.y), rc.getMapHeight() - 1));
+        dir = Pathfinder.getMoveDir(rc, inBounds);
+        
+        if (rc.canMove(dir)) {
+            rc.move(dir);
+        }
+
+        //Comms stuff
+        Comms.updateSector(rc, turnCount);
+        int deltaIncome = income - prevIncome;
+        //index 49 is global income
+        int currentIncome = rc.readSharedArray(49);
+        rc.writeSharedArray(49, Math.max(0, currentIncome + deltaIncome));
+        prevIncome = income;
+
+        //self report alive or ded
+        boolean currentHpThresh = (double)rc.getHealth()/rc.getType().getMaxHealth(1) > 0.2;
+        if (!currentHpThresh && aboveHpThresh) {
+            rc.writeSharedArray(50, rc.readSharedArray(50) - 1);
+        } else if (currentHpThresh && !aboveHpThresh) {
+            rc.writeSharedArray(50, rc.readSharedArray(50) + 1);
+        }
+        aboveHpThresh = currentHpThresh;
+        turnsAlive++;
+    }
+}
---
+++ src/v8/BetterSoldier.java
@@ -0,0 +1,253 @@
+package v8;
+
+import battlecode.common.*;
+
+public class BetterSoldier extends RobotPlayer {
+
+  static int turnsAlive = 0;
+  static boolean aboveHpThresh = true;
+  static boolean notRepaired = false;
+  static MapLocation[] sectorMdpts = new MapLocation[49];
+  static MapLocation bestTgtSector = null;
+  static MapLocation closestFriendlyArchon = null;
+  static MapLocation scoutTgt = null;
+
+  static Direction stallOnGoodRubble(RobotController rc) throws GameActionException {
+    MapLocation src = rc.getLocation();
+    int currRubble = rc.senseRubble(src);
+      int minRubble = currRubble;
+      MapLocation minRubbleLoc = src;
+      if (currRubble > 0) {
+        for (Direction d : Direction.allDirections()) {
+          MapLocation test = src.add(d);
+          if (rc.onTheMap(test) && !rc.isLocationOccupied(test) && rc.canSenseLocation(test)) {
+            int rubbleHere = rc.senseRubble(test);
+            if (rubbleHere <= minRubble) {
+              minRubble = rubbleHere;
+              minRubbleLoc = test;
+            }
+          }
+        }
+      }
+      return src.directionTo(minRubbleLoc);
+  }
+
+  static boolean isHostile(RobotInfo unit) {
+    return (unit.getType() == RobotType.SOLDIER || unit.getType() == RobotType.SAGE 
+    || unit.getType() == RobotType.WATCHTOWER);
+  }
+
+  /**
+   * Run a single turn for a Soldier.
+   * This code is wrapped inside the infinite loop in run(), so it is called once per turn.
+   */
+  static void runSoldier(RobotController rc) throws GameActionException {
+    MapLocation src = rc.getLocation();
+    int radius = rc.getType().actionRadiusSquared;
+    int senseRadius = rc.getType().visionRadiusSquared;
+    Team friendly = rc.getTeam();
+    Team opponent = rc.getTeam().opponent();
+    RobotInfo[] enemies = rc.senseNearbyRobots(senseRadius, opponent);
+    RobotInfo[] friendlies = rc.senseNearbyRobots(senseRadius, friendly);
+    RobotInfo attackTgt = null;
+    RobotInfo inVisionTgt = null;
+    int nearbyDamage = 0;
+    int nearbyDamageHealth = 0;
+    int rubbleThreshold = rc.senseRubble(rc.getLocation()) + 20;
+    Direction dir = null;
+    boolean frontline = true;
+
+    if (turnsAlive == 0) {
+      for (int i = 48; i >= 0; i --) {
+          sectorMdpts[i] = Comms.sectorMidpt(rc, i);
+      }
+      int scoutPattern = rc.readSharedArray(50) % 2;
+      if (scoutPattern == 0) {
+        scoutTgt = sectorMdpts[rng.nextInt(49)];
+      } else {
+        int designatedLoc = rng.nextInt(5);
+        switch (designatedLoc) {
+            case 0:
+                scoutTgt = new MapLocation(0, 0);
+                break;
+            case 1:
+                scoutTgt = new MapLocation(rc.getMapWidth() - 1, 0);
+                break;
+            case 2:
+                scoutTgt = new MapLocation(0, rc.getMapHeight() - 1);
+                break;
+            case 3:
+                scoutTgt = new MapLocation(rc.getMapWidth() - 1, rc.getMapHeight() - 1);
+                break;
+            case 4:
+                scoutTgt = new MapLocation(rc.getMapWidth()/2, rc.getMapHeight()/2);
+                break;
+            }
+        }
+    }
+
+    //if we reach full health then we are repaired
+    if (rc.getHealth() == RobotType.SOLDIER.getMaxHealth(rc.getLevel())) {
+      notRepaired = false;
+    }
+
+    //pre move attack and enemy assessment
+    int lowestHPTgt = 9999;
+    int lowestInVisionHPTgt = 9999;
+    if (enemies.length > 0) {
+      for (int i = enemies.length - 1; i >= 0; i --) {
+        RobotInfo enemy = enemies[i];
+        if (enemy.getLocation().distanceSquaredTo(src) <= radius) {
+          if (attackTgt == null) {
+            lowestHPTgt = enemy.getHealth();
+            attackTgt = enemy;
+          } else if (isHostile(enemy) && !isHostile(attackTgt)) {
+            lowestHPTgt = enemy.getHealth();
+            attackTgt = enemy;
+          } else if (enemy.getHealth() < lowestHPTgt 
+          && ((isHostile(enemy) && isHostile(attackTgt)) || (!isHostile(enemy) && !isHostile(attackTgt)))) {
+            lowestHPTgt = enemy.getHealth();
+            attackTgt = enemy;
+          }
+        } else {
+          if (inVisionTgt == null) {
+            lowestInVisionHPTgt = enemy.getHealth();
+            inVisionTgt = enemy;
+          } else if (isHostile(enemy) && !isHostile(inVisionTgt)) {
+            lowestInVisionHPTgt = enemy.getHealth();
+            inVisionTgt = enemy;
+          } else if (enemy.getHealth() < lowestInVisionHPTgt 
+            && ((isHostile(enemy) && isHostile(inVisionTgt)) || (!isHostile(enemy) && !isHostile(inVisionTgt)))) {
+            lowestInVisionHPTgt = enemy.getHealth();
+            inVisionTgt = enemy;
+          }
+        }
+      }
+      if (attackTgt != null && inVisionTgt == null) {
+        inVisionTgt = attackTgt;
+      }
+
+      if (attackTgt != null && rc.canAttack(attackTgt.location)) {
+        MapLocation toAttack = attackTgt.location;
+        rc.attack(toAttack);
+      }
+    }
+
+    //assess teammates
+    for (int i = friendlies.length - 1; i >= 0; i --) {
+        RobotInfo friend = friendlies[i];
+        if (isHostile(friend)) {
+            nearbyDamage ++;
+            nearbyDamageHealth += friend.getHealth();
+            if (frontline && attackTgt != null 
+            && friend.location.distanceSquaredTo(attackTgt.location) < src.distanceSquaredTo(attackTgt.location)) {
+                frontline = false;
+            }
+        }
+    }
+    double averageHealth = (double)nearbyDamageHealth/nearbyDamage;
+
+    //comms access every other turn for bytecode reduction
+    if (turnCount % 3 == 0 || turnsAlive == 0) {
+        bestTgtSector = null;
+        double highScore = 0;
+        for (int i = 48; i >= 0; i --) {
+            int[] sector = Comms.readSectorInfo(rc, i);
+            if (sector[0] == 1 && (closestFriendlyArchon == null 
+            || sectorMdpts[i].distanceSquaredTo(src) < closestFriendlyArchon.distanceSquaredTo(src))) {
+                closestFriendlyArchon = sectorMdpts[i];
+            }
+            if (sector[1] == 1 || sector[3] > 0) {
+                double currentScore = (50.0*sector[1] + sector[3])/Math.sqrt(src.distanceSquaredTo(sectorMdpts[i]));
+                if (currentScore > highScore) {
+                    bestTgtSector = sectorMdpts[i];
+                }
+            }
+        }
+    }
+
+    //if you can see the scout target sector mdpt, randomize and go to somewhere else
+    if (src.distanceSquaredTo(scoutTgt) <= senseRadius) {
+      scoutTgt = sectorMdpts[rng.nextInt(49)];
+    }
+
+    int repairThresh = (int)(15 + (-1.0*(Math.abs(src.x - closestFriendlyArchon.x) + Math.abs(src.y - closestFriendlyArchon.y)))/12);
+    //main movement loop
+    //if low enough hp run back to heal
+    if ((notRepaired || rc.getHealth() <= repairThresh) && src.distanceSquaredTo(closestFriendlyArchon) >= 3) {
+      dir = Pathfinder.getMoveDir(rc, closestFriendlyArchon);
+      notRepaired = true;
+    //if mid hp comparatively or no cd, shuffle
+    } else if (attackTgt != null && ((frontline && rc.getHealth() < averageHealth && rc.getHealth() < 15 + repairThresh) 
+              || !rc.isActionReady())) {
+      Direction opposite = src.directionTo(inVisionTgt.location).opposite();
+      MapLocation runawayTgt = src.add(opposite).add(opposite);
+      runawayTgt = new MapLocation(Math.min(Math.max(0, runawayTgt.x), rc.getMapWidth() - 1), 
+      Math.min(Math.max(0, runawayTgt.y), rc.getMapHeight() - 1));
+      dir = Pathfinder.getMoveDir(rc, runawayTgt);
+      MapLocation kitingTgt = src.add(dir);
+      if (rc.senseRubble(kitingTgt) > rubbleThreshold) {
+          dir = stallOnGoodRubble(rc);
+      }
+    //if enough soldiers nearby advance to make space
+    } else if (nearbyDamage > 4 && attackTgt != null) {
+      dir = Pathfinder.getMoveDir(rc, attackTgt.location);
+      MapLocation kitingTgt = src.add(dir);
+      if (rc.senseRubble(kitingTgt) > rubbleThreshold) {
+        dir = stallOnGoodRubble(rc);
+      }
+    //otherwise if there is an enemy sector go to best scored one
+    } else if (bestTgtSector != null) {
+      dir = Pathfinder.getMoveDir(rc, bestTgtSector);
+    //otherwise scout same as miner
+    } else {
+      dir = Pathfinder.getMoveDir(rc, scoutTgt);
+    }
+
+    //move
+    if (dir != null && rc.canMove(dir)) {
+      rc.move(dir);
+    }
+
+    //post move attack if available
+    if (rc.isActionReady()) {
+      attackTgt = null;
+      enemies = rc.senseNearbyRobots(senseRadius, opponent);
+      lowestHPTgt = 9999;
+      if (enemies.length > 0) {
+        for (int i = enemies.length - 1; i >= 0; i --) {
+          RobotInfo enemy = enemies[i];
+          if (enemy.getLocation().distanceSquaredTo(src) <= radius) {
+            if (attackTgt == null) {
+              lowestHPTgt = enemy.getHealth();
+              attackTgt = enemy;
+            } else if (isHostile(enemy) && !isHostile(attackTgt)) {
+              lowestHPTgt = enemy.getHealth();
+              attackTgt = enemy;
+            } else if (enemy.getHealth() < lowestHPTgt 
+            && ((isHostile(enemy) && isHostile(attackTgt)) || (!isHostile(enemy) && !isHostile(attackTgt)))) {
+              lowestHPTgt = enemy.getHealth();
+              attackTgt = enemy;
+            }
+          }
+        }
+        if (attackTgt != null && rc.canAttack(attackTgt.location)) {
+          MapLocation toAttack = attackTgt.location;
+          rc.attack(toAttack);
+        }
+      }
+    }
+
+    turnsAlive++;
+
+    //update comms
+    Comms.updateSector(rc, turnCount);
+    boolean currentHpThresh = (double)rc.getHealth()/rc.getType().getMaxHealth(1) > 0.2;
+    if (!currentHpThresh && aboveHpThresh) {
+        rc.writeSharedArray(51, rc.readSharedArray(51) - 1);
+    } else if (currentHpThresh && !aboveHpThresh) {
+        rc.writeSharedArray(51, rc.readSharedArray(51) + 1);
+    }
+    aboveHpThresh = currentHpThresh;
+  }
+}

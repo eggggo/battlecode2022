750d10da03dbb8a404de516d98d6c5cc8131a078
Andrew Li <achyt28@gmail.com>
2022-01-24 14:49:16 -0500

    sage attack/move simple decision making

--- src/mainBot/Sage.java
+++ src/mainBot/Sage.java
@@ -59,45 +59,46 @@ public class Sage extends RobotPlayer{
         RobotInfo inVisionTgt = null;
         int unitHP = 0;
         int buildingHP = 0;
-        int enemiesKilled = 0;
+        int unitsKilled = 0;
+        int buildingsKilled = 0;
         int rubbleThreshold = rc.senseRubble(rc.getLocation()) + 20;
         boolean frontline = true;
         int nearbyDamage = 0;
         int nearbyDamageHealth = 0;
 
         if (turnsAlive == 0) {
-            for (int i = 48; i >= 0; i --) {
+            for (int i = 48; i >= 0; i--) {
                 sectorMdpts[i] = Comms.sectorMidpt(rc, i);
             }
             int scoutPattern = rc.readSharedArray(50) % 2;
             if (scoutPattern == 0) {
-              scoutTgt = sectorMdpts[rng.nextInt(49)];
+                scoutTgt = sectorMdpts[rng.nextInt(49)];
             } else {
-              int designatedLoc = rng.nextInt(5);
-              switch (designatedLoc) {
-                  case 0:
-                      scoutTgt = new MapLocation(0, 0);
-                      break;
-                  case 1:
-                      scoutTgt = new MapLocation(rc.getMapWidth() - 1, 0);
-                      break;
-                  case 2:
-                      scoutTgt = new MapLocation(0, rc.getMapHeight() - 1);
-                      break;
-                  case 3:
-                      scoutTgt = new MapLocation(rc.getMapWidth() - 1, rc.getMapHeight() - 1);
-                      break;
-                  case 4:
-                      scoutTgt = new MapLocation(rc.getMapWidth()/2, rc.getMapHeight()/2);
-                      break;
-                  }
-              }
+                int designatedLoc = rng.nextInt(5);
+                switch (designatedLoc) {
+                    case 0:
+                        scoutTgt = new MapLocation(0, 0);
+                        break;
+                    case 1:
+                        scoutTgt = new MapLocation(rc.getMapWidth() - 1, 0);
+                        break;
+                    case 2:
+                        scoutTgt = new MapLocation(0, rc.getMapHeight() - 1);
+                        break;
+                    case 3:
+                        scoutTgt = new MapLocation(rc.getMapWidth() - 1, rc.getMapHeight() - 1);
+                        break;
+                    case 4:
+                        scoutTgt = new MapLocation(rc.getMapWidth() / 2, rc.getMapHeight() / 2);
+                        break;
+                }
+            }
         }
 
         int lowestHPTgt = 9999;
         int lowestInVisionHPTgt = 9999;
         if (enemies.length > 0) {
-            for (int i = enemies.length - 1; i >= 0; i --) {
+            for (int i = enemies.length - 1; i >= 0; i--) {
                 RobotInfo enemy = enemies[i];
                 if (enemy.getLocation().distanceSquaredTo(src) <= radius) {
                     if (attackTgt == null ||
@@ -113,10 +114,13 @@ public class Sage extends RobotPlayer{
                         if (enemy.getMode() == RobotMode.TURRET) {
                             buildingHP += enemy.getType().getMaxHealth(enemy.getLevel());
                         }
+                        if (enemy.getHealth() <= enemy.getType().getMaxHealth(enemy.getLevel()) * AnomalyType.FURY.sagePercentage) {
+                            buildingsKilled++;
+                        }
                     } else {
                         unitHP += enemy.getType().getMaxHealth(1);
-                        if (enemy.getHealth() <= enemy.getType().getMaxHealth(1)*0.22) {
-                            enemiesKilled ++;
+                        if (enemy.getHealth() <= enemy.getType().getMaxHealth(1) * AnomalyType.CHARGE.sagePercentage) {
+                            unitsKilled++;
                         }
                     }
                 } else {
@@ -136,13 +140,13 @@ public class Sage extends RobotPlayer{
         }
 
         //assess teammates
-        for (int i = friendlies.length - 1; i >= 0; i --) {
+        for (int i = friendlies.length - 1; i >= 0; i--) {
             RobotInfo friend = friendlies[i];
             if (isHostile(friend)) {
-                nearbyDamage ++;
+                nearbyDamage++;
                 nearbyDamageHealth += friend.getHealth();
-                if (frontline && attackTgt != null 
-                && friend.location.distanceSquaredTo(attackTgt.location) < src.distanceSquaredTo(attackTgt.location)) {
+                if (frontline && attackTgt != null
+                        && friend.location.distanceSquaredTo(attackTgt.location) < src.distanceSquaredTo(attackTgt.location)) {
                     frontline = false;
                 }
             }
@@ -150,23 +154,23 @@ public class Sage extends RobotPlayer{
                 buildingHP -= friend.getType().getMaxHealth(friend.level);
             }
         }
-        double averageHealth = (double)nearbyDamageHealth/nearbyDamage;
+        double averageHealth = (double) nearbyDamageHealth / nearbyDamage;
 
-            //comms access every 3rd turn for bytecode reduction
+        //comms access every 3rd turn for bytecode reduction
         if (turnCount % 3 == 0 || turnsAlive == 0) {
             bestTgtSector = null;
             closestFriendlyArchon = null;
             double highScore = 0;
-            for (int i = 48; i >= 0; i --) {
+            for (int i = 48; i >= 0; i--) {
                 int homeArchon = Comms.readSectorInfo(rc, i, 0);
                 int enemyArchon = Comms.readSectorInfo(rc, i, 1);
                 int enemyInSector = Comms.readSectorInfo(rc, i, 3);
-                if (homeArchon == 1 && (closestFriendlyArchon == null 
-                || sectorMdpts[i].distanceSquaredTo(src) < closestFriendlyArchon.distanceSquaredTo(src))) {
+                if (homeArchon == 1 && (closestFriendlyArchon == null
+                        || sectorMdpts[i].distanceSquaredTo(src) < closestFriendlyArchon.distanceSquaredTo(src))) {
                     closestFriendlyArchon = sectorMdpts[i];
                 }
                 if (enemyArchon == 1 || enemyInSector > 0) {
-                    double currentScore = (10.0*enemyArchon + enemyInSector)/(src.distanceSquaredTo(sectorMdpts[i]));
+                    double currentScore = (10.0 * enemyArchon + enemyInSector) / (src.distanceSquaredTo(sectorMdpts[i]));
                     if (currentScore > highScore) {
                         bestTgtSector = sectorMdpts[i];
                         highScore = currentScore;
@@ -176,7 +180,7 @@ public class Sage extends RobotPlayer{
         }
 
         //if we reach full health then we are repaired
-        if (rc.getHealth() >= RobotType.SAGE.getMaxHealth(rc.getLevel())/2) {
+        if (rc.getHealth() >= RobotType.SAGE.getMaxHealth(rc.getLevel()) / 2) {
             notRepaired = false;
         }
 
@@ -190,44 +194,47 @@ public class Sage extends RobotPlayer{
         if ((notRepaired || rc.getHealth() <= 22) && src.distanceSquaredTo(closestFriendlyArchon) >= 3) {
             dir = Pathfinder.getMoveDir(rc, closestFriendlyArchon, prev5Spots);
             notRepaired = true;
-        //if mid hp comparatively or no cd, shuffle
-        } else if (inVisionTgt != null && isHostile(inVisionTgt) && ((frontline && rc.getHealth() < averageHealth && rc.getHealth() <= 45) 
+            //if mid hp comparatively or no cd, shuffle
+        } else if (inVisionTgt != null && isHostile(inVisionTgt) && ((frontline && rc.getHealth() < averageHealth && rc.getHealth() <= 45)
                 || !rc.isActionReady())) {
             int dx = inVisionTgt.location.x - src.x;
             int dy = inVisionTgt.location.y - src.y;
-            MapLocation runawayTgt = new MapLocation(Math.min(Math.max(0, src.x - dx), rc.getMapWidth() - 1), 
-            Math.min(Math.max(0, src.y - dy), rc.getMapHeight() - 1));
+            MapLocation runawayTgt = new MapLocation(Math.min(Math.max(0, src.x - dx), rc.getMapWidth() - 1),
+                    Math.min(Math.max(0, src.y - dy), rc.getMapHeight() - 1));
             prev5Spots = new MapLocation[5];
             dir = Pathfinder.getMoveDir(rc, runawayTgt, prev5Spots);
             MapLocation kitingTgt = src.add(dir);
             if (rc.senseRubble(kitingTgt) > rubbleThreshold) {
                 dir = stallOnGoodRubble(rc);
             }
-        //if enough soldiers nearby advance to make space
+            //if enough soldiers nearby advance to make space
         } else if (inVisionTgt != null && (nearbyDamage >= 4 || src.distanceSquaredTo(inVisionTgt.location) > 14)) {
             dir = Pathfinder.getMoveDir(rc, inVisionTgt.location, prev5Spots);
             MapLocation kitingTgt = src.add(dir);
             if (rc.senseRubble(kitingTgt) > rubbleThreshold) {
                 dir = stallOnGoodRubble(rc);
             }
-        //otherwise if there is an enemy sector go to best scored one
+            //otherwise if there is an enemy sector go to best scored one
         } else if (bestTgtSector != null) {
             dir = Pathfinder.getMoveDir(rc, bestTgtSector, prev5Spots);
             if (src.distanceSquaredTo(bestTgtSector) < 50 && rc.senseRubble(src.add(dir)) > rubbleThreshold) {
                 dir = stallOnGoodRubble(rc);
             }
-          //otherwise scout same as miner
+            //otherwise scout same as miner
         } else {
             dir = Pathfinder.getMoveDir(rc, scoutTgt, prev5Spots);
         }
 
 
         //move-attack or attack-move decision making calculations
-        if(rc.isActionReady()) {
-            MapLocation togo = src.add(dir);
+        MapLocation togo = null;
+        int unitHP2 = 0;
+        int buildingHP2 = 0;
+        int unitsKilled2 = 0;
+        int buildingsKilled2 = 0;
+        if (rc.isActionReady()) {
+            togo = src.add(dir);
             RobotInfo[] enemies2 = rc.senseNearbyRobots(togo, senseRadius, opponent);
-            int buildingHP2 = 0;
-            int unitHP2 = 0;
             if (enemies2.length > 0) {
                 for (int i = enemies2.length - 1; i >= 0; i--) {
                     RobotInfo enemy = enemies2[i];
@@ -236,95 +243,110 @@ public class Sage extends RobotPlayer{
                             if (enemy.getMode() == RobotMode.TURRET) {
                                 buildingHP2 += enemy.getType().getMaxHealth(enemy.getLevel());
                             }
+                            if (enemy.getHealth() <= enemy.getType().getMaxHealth(enemy.getLevel()) * AnomalyType.FURY.sagePercentage) {
+                                buildingsKilled2++;
+                            }
                         } else {
                             unitHP2 += enemy.getType().getMaxHealth(1);
+                            if (enemy.getHealth() <= enemy.getType().getMaxHealth(1) * AnomalyType.CHARGE.sagePercentage) {
+                                unitsKilled2++;
+                            }
                         }
                     }
                 }
             }
-
-            if (rc.senseRubble(src) < rc.senseRubble(togo) && unitHP > unitHP2) {
-                if (AnomalyType.CHARGE.sagePercentage * unitHP >= RobotType.SAGE.getDamage(1)
-                        && rc.canEnvision(AnomalyType.CHARGE)) {
-                    rc.envision(AnomalyType.CHARGE);
-                }
-            }
-            else if (rc.senseRubble(src) < rc.senseRubble(togo) && buildingHP > buildingHP2){
-
-            }
         }
 
 
-
+        boolean chargeLater = false, furyLater = false, attackLater = false;
         //maximize damage done
-        if ( (AnomalyType.CHARGE.sagePercentage * unitHP >= RobotType.SAGE.getDamage(1) || enemiesKilled > 1)
-          && rc.canEnvision(AnomalyType.CHARGE)) {
-            rc.envision(AnomalyType.CHARGE);
-        } else if ((AnomalyType.FURY.sagePercentage * buildingHP >= 60)
-          && rc.canEnvision(AnomalyType.FURY)) {
-            rc.envision(AnomalyType.FURY);
+        if ((AnomalyType.CHARGE.sagePercentage * unitHP >= RobotType.SAGE.getDamage(1) || unitsKilled > 1)
+                && rc.canEnvision(AnomalyType.CHARGE)) {
+            if((unitHP2 >= unitHP && unitsKilled2 >= unitsKilled) && rc.senseRubble(src) >= rc.senseRubble(togo)){
+                chargeLater = true;
+            }
+            else{
+                rc.envision(AnomalyType.CHARGE);
+            }
+        } else if ((AnomalyType.FURY.sagePercentage * buildingHP >= 60 || buildingsKilled > 0)
+                && rc.canEnvision(AnomalyType.FURY)) {
+            if((buildingHP2 >= buildingHP && buildingsKilled2 >= buildingsKilled) && rc.senseRubble(src) >= rc.senseRubble(togo)){
+                furyLater = true;
+            }
+            else{
+                rc.envision(AnomalyType.FURY);
+            }
         } else if (attackTgt != null && rc.canAttack(attackTgt.location)) {
-            MapLocation toAttack = attackTgt.location;
-            rc.attack(toAttack);
+            if(rc.senseRubble(src) >= rc.senseRubble(togo) && togo.distanceSquaredTo(attackTgt.location) <= radius){
+                attackLater = true;
+            }
+            else{
+                MapLocation toAttack = attackTgt.location;
+                rc.attack(toAttack);
+            }
         }
 
         //determine whether to attack here or after moving
         if (dir != null && rc.canMove(dir)) {
             rc.move(dir);
             prev5Spots[currentOverrideIndex] = rc.getLocation();
-            currentOverrideIndex  = (currentOverrideIndex + 1) % 5;
+            currentOverrideIndex = (currentOverrideIndex + 1) % 5;
         }
 
         //post move attack if available
-        if (rc.isActionReady() ) { //&& rc.senseRubble(rc.getLocation()) < rubbleThreshold) {
+        if (rc.isActionReady()) { //&& rc.senseRubble(rc.getLocation()) < rubbleThreshold) {
             attackTgt = null;
             src = rc.getLocation();
             enemies = rc.senseNearbyRobots(senseRadius, opponent);
             lowestHPTgt = 9999;
             buildingHP = 0;
             unitHP = 0;
-            enemiesKilled = 0;
+            unitsKilled = 0;
+            buildingsKilled = 0;
             if (enemies.length > 0) {
-            for (int i = enemies.length - 1; i >= 0; i --) {
-                RobotInfo enemy = enemies[i];
-                if (enemy.getLocation().distanceSquaredTo(src) <= radius) {
-                    if (attackTgt == null ||
-                            isHostile(enemy) && !isHostile(attackTgt) ||
-                            isHostile(enemy) == isHostile(attackTgt) && enemy.getHealth() < lowestHPTgt) {
-                        //note: && has higher precedence than ||
+                for (int i = enemies.length - 1; i >= 0; i--) {
+                    RobotInfo enemy = enemies[i];
+                    if (enemy.getLocation().distanceSquaredTo(src) <= radius) {
+                        if (attackTgt == null ||
+                                isHostile(enemy) && !isHostile(attackTgt) ||
+                                isHostile(enemy) == isHostile(attackTgt) && enemy.getHealth() < lowestHPTgt) {
+                            //note: && has higher precedence than ||
 
-                        lowestHPTgt = enemy.getHealth();
-                        attackTgt = enemy;
-                    }
-
-                    if ((enemy.getType() == RobotType.ARCHON || enemy.getType() == RobotType.WATCHTOWER || enemy.getType() == RobotType.LABORATORY)) {
-                        if (enemy.getMode() == RobotMode.TURRET) {
-                            buildingHP += enemy.getType().getMaxHealth(enemy.getLevel());
+                            lowestHPTgt = enemy.getHealth();
+                            attackTgt = enemy;
                         }
-                    } else {
-                        unitHP += enemy.getType().getMaxHealth(1);
-                        if (enemy.getHealth() <= enemy.getType().getMaxHealth(1)*0.22) {
-                            enemiesKilled ++;
+
+                        if ((enemy.getType() == RobotType.ARCHON || enemy.getType() == RobotType.WATCHTOWER || enemy.getType() == RobotType.LABORATORY)) {
+                            if (enemy.getMode() == RobotMode.TURRET) {
+                                buildingHP += enemy.getType().getMaxHealth(enemy.getLevel());
+                            }
+                            if (enemy.getHealth() <= enemy.getType().getMaxHealth(enemy.getLevel()) * AnomalyType.FURY.sagePercentage) {
+                                buildingsKilled++;
+                            }
+                        } else {
+                            unitHP += enemy.getType().getMaxHealth(1);
+                            if (enemy.getHealth() <= enemy.getType().getMaxHealth(1) * AnomalyType.CHARGE.sagePercentage) {
+                                unitsKilled++;
+                            }
                         }
                     }
                 }
-            }
-            //maximize damage done
-            if ( (AnomalyType.CHARGE.sagePercentage * unitHP >= RobotType.SAGE.getDamage(1) || enemiesKilled > 1)
-                    && rc.canEnvision(AnomalyType.CHARGE)) {
-                rc.envision(AnomalyType.CHARGE);
-            } else if ((AnomalyType.FURY.sagePercentage * buildingHP >= 60)
-                    && rc.canEnvision(AnomalyType.FURY)) {
-                rc.envision(AnomalyType.FURY);
-            } else if (attackTgt != null && rc.canAttack(attackTgt.location)) {
-                MapLocation toAttack = attackTgt.location;
-                rc.attack(toAttack);
-            }
+                //maximize damage done
+                if (chargeLater || (AnomalyType.CHARGE.sagePercentage * unitHP >= RobotType.SAGE.getDamage(1) || unitsKilled > 1)
+                        && rc.canEnvision(AnomalyType.CHARGE)) {
+                    rc.envision(AnomalyType.CHARGE);
+                } else if (furyLater || (AnomalyType.FURY.sagePercentage * buildingHP >= 60) || buildingsKilled > 0
+                        && rc.canEnvision(AnomalyType.FURY)) {
+                    rc.envision(AnomalyType.FURY);
+                } else if (attackTgt != null && rc.canAttack(attackTgt.location)) {
+                    MapLocation toAttack = attackTgt.location;
+                    rc.attack(toAttack);
+                }
             }
         }
 
         Comms.updateSector(rc);
         Comms.updateTypeCount(rc);
-        turnsAlive ++;
+        turnsAlive++;
     }
 }
